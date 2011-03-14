a776dc214fb587707008653d457a738f951001f5
Junichiro Kita <junichiro.kita@gmail.com>
2011-03-14 23:29:27 +0900

    import from http://www.clear-code.com/blog/2011/2/16.html

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,27 +1,102 @@
-;;; Japanese
-(set-language-environment 'Japanese)
-(prefer-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
+;;; Locale
+(set-locale-environment (or (getenv "LANG")
+			    "ja_JP.UTF-8"))
 
-;(setq default-input-method "MacOSX")
+;;; Backup & Autosave
+(setq backup-inhibited t)
+(setq delete-auto-save-files t)
+(setq auto-save-list-file-prefix "~/.autosave/.saves-")
 
-;;; Battery
-(display-battery-mode t)
 
 ;;; C-h <-> DEL
 (keyboard-translate ?\C-h ?\C-?)
+
+
+;;; Visit image files as images
+(auto-image-file-mode t)
+
+
+;;; Hide menu bar and tool bar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+
+;;; Turn blinking cursor mode off
+(blink-cursor-mode nil)
+
+
+;;; No limit while printing value in `eval-expression'
+(setq eval-expression-print-length nil)
+
+
+;;; Show paren and trailing whitespaces
+(show-paren-mode t)
+(setq show-paren-style 'mixed)
+(setq-default show-trailing-whitespace t)    
+
+
+;;; Show line and column number
+(line-number-mode t)
+(column-number-mode t)
+
+
+;;; Automatically save place in each file
+(require 'saveplace)
+(setq-default save-place t)
+
+
+;;; Kill line and newline
+(setq kill-whole-line t)
+(setq require-final-newline t)
+(setq next-line-add-newlines nil)
+
+
+;;; Completion
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+(partial-completion-mode t)
+(icomplete-mode 1)
+
+
+;;; History
+(setq history-length 10000)
+(setq recentf-max-saved-items 10000)
+(savehist-mode 1)
+
+
+;;; Automatic file compression/decompression
+(auto-compression-mode t)
+
+
+;;; dired
+(add-hook 'dired-load-hook
+	  '(lambda ()
+	     (load "dired-x")
+	     (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)))
+(autoload 'wdired-change-to-wdired-mode "wdired")
+
+
+
+;;; Unique buffer name
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+
+;;; Battery
+(display-battery-mode t)
 
 
 ;;; font-set
 (if (eq window-system 'mac)
     (progn
       (require 'carbon-font)
-      (fixed-width-set-fontset 'hirakaku_w3 12)))
+      (fixed-width-set-fontset 'hiramaru 12)))
 
 (setq default-frame-alist
-       (append '((active-alpha . 0.80)
+       (append '((active-alpha . 1.00)
 		 (inactive-alpha . 0.80))
                default-frame-alist))
+
 
 ;;; tDiary
 (add-to-list 'load-path (expand-file-name "~/Documents/Emacsen/apel"))
